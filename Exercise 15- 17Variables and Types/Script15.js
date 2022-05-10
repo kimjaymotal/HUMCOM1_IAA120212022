a5ddd04f0db7275f6b307dbc98b919a836bd87fd
kimjaymotal <98306096+kimjaymotal@users.noreply.github.com>
2022-05-10 20:05:52 +0800

    Add files via upload

---
+++ Exercise 15- 17Variables and Types/Script15.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var name = document.getElementById("fname").value;
+    var age = document.getElementById("age").value;
+    var num = document.getElementById("num").value;
+    var email = document.getElementById("email").value;
+    var Mail = document.getElementById("Mail").value;
+    var prog = document.getElementById("prog").value;
+    var id = document.getElementById("id").value;
+    var year = document.getElementById("year").value;
+    var sect = document.getElementById("sect").value;
+     alert(name+'\n'+age+ '\n'+num+'\n'+email+'\n'+Mail+'\n'+prog+'\n'+id+'\n'+year+'\n'+sect);
+   
+  }
